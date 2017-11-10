e3f369b2fd3ab88ccfb90215ede8e07d5aa81cec
umutyazgan <umut.yaz@gmail.com>
2017-11-10 16:26:02 +0300

    Fix install script

--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-ln -s /etc/nginx/sites-available/hapra /etc/nginx/sites-enabled/hapra
 rm /etc/nginx/sites-enabled/default
 echo "Would you like to use HTTP Basic Auth?[y/N]"
 read auth
 if [ $auth == y ]; then
-	mv hapra_auth.nginx /etc/nginx/sites-available/hapra
+	cp hapra_auth.nginx /etc/nginx/sites-available/hapra
+	chown root:root /etc/nginx/sites-available/hapra
+	rm /etc/nginx/.haprapasswd
 	echo "Username: "
 	read username
 	echo "Password: "
 	read password
-	$password_hash = {openssl passwd -apr1 $password}
-	echo $password_hash
-	echo -n "${username}:" >> /etc/nginx/.htpasswd
-	echo -n "$(openssl passwd -apr1 $password_hash)" >> /etc/nginx/.htpasswd
+	echo $password
+	echo -n "${username}:" >> /etc/nginx/.haprapasswd
+	echo -n "$(openssl passwd -apr1 $password)" >> /etc/nginx/.haprapasswd
 else
-	mv hapra.nginx /etc/nginx/sites-available/hapra
+	cp hapra.nginx /etc/nginx/sites-available/hapra
+	chown root:root /etc/nginx/sites-available/hapra
 fi
+ln -s /etc/nginx/sites-available/hapra /etc/nginx/sites-enabled/hapra
+systemctl restart nginx
